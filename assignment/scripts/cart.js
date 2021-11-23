20b5dfa55b0c122d04d8ef96f6c0fdba71e6b31a
Kayode Adebanjo <kay.adebanjo@gmail.com>
2021-11-22 19:00:01 -0600

    part 3 completed without strech

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,29 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+const maxItem = 5;
+
+function addItem(item){
+  basket.push(item);
+  return true;
+}
+console.log(addItem ('apple'));
+console.log(addItem ('strawberry'));
+console.log(addItem ('grape'));
+console.log(basket);
+
+//creating function for listItems
+function listItems(){
+  for (let item of basket) {
+    console.log(item);
+  }
+}
+//listItems on new line in the console
+listItems()
+
+function empty(rice){
+  basket = [rice];
+}
+empty()
+console.log(`Basket is now ${basket}`);
